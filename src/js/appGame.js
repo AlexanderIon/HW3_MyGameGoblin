2ce11eeeabe711e803b53a25669be82b65a356d2
AlexanderIon <tov.sandro@mail.ru>
2022-12-13 12:46:15 +0300

    ready

--- src/js/appGame.js
+++ src/js/appGame.js
@@ -1,47 +1,42 @@
-const { FieldGame } = require('./fieldGame/fielGame.js');
+const { default: FieldGame } = require('./fieldGame/fielGame.js');
 const { default: Goblin } = require('./goblin/goblin.js');
 
 document.addEventListener('DOMContentLoaded', () => {
   const fiedls = document.querySelector('.fieldGame');
   const btn = document.querySelector('.activeGoblinBtn');
-  let count = document.querySelector('.count')
-  let miss = document.querySelector('.miss')
+  const count = document.querySelector('.count');
+  const miss = document.querySelector('.miss');
   const gonlin = new Goblin(fiedls);
-  const boxes = new FieldGame('.fieldGame')
+  const boxes = new FieldGame('.fieldGame');
   let statusGame = false;
-  let game = 0
+  // const game = 0;
   // btn.style.cursor = " grab"
   // count.textContent = boxes.count
-  
+
   btn.addEventListener('click', () => {
-    
-    statusGame =! statusGame;
-    
-    if (statusGame){
-      btn.style.backgroundColor = ['red']
-      btn.textContent = "STOP"
-    }else{
-      btn.style.backgroundColor = ['green']
-      btn.textContent = "STAR"
+    statusGame = !statusGame;
+
+    if (statusGame) {
+      btn.style.backgroundColor = ['red'];
+      btn.textContent = 'STOP';
+    } else {
+      btn.style.backgroundColor = ['green'];
+      btn.textContent = 'STAR';
     }
-    
 
-      const game = setInterval(() => {
-        gonlin.movingdGoblin();
-        count.textContent = `СЧЕТ : ${boxes.count}`
-        miss.textContent = `ПРОМАХ : ${boxes.miss}`
+    const game = setInterval(() => {
+      gonlin.movingdGoblin();
+      count.textContent = `СЧЕТ : ${boxes.count}`;
+      miss.textContent = `ПРОМАХ : ${boxes.miss}`;
 
-        if (!statusGame || boxes.miss > 4) {
-          boxes.count = 0;
-          boxes.miss = 0;
-          btn.style.backgroundColor = ['green']
-          btn.textContent = "START"
-          clearInterval(game)
-        }
-               
-      }, 2400);
-   });
+      if (!statusGame || boxes.miss > 4) {
+        boxes.count = 0;
+        boxes.miss = 0;
+        btn.style.backgroundColor = ['green'];
+        btn.textContent = 'START';
+        clearInterval(game);
+      }
+    }, 1000);
+  });
   //  console.log(boxes.element)
-
-
 });
